248116f0b5b424191565bc30453a5b63d201e6d1
Ma, Alan <alan.ma@disney.com>
2015-01-16 16:00:34 -0800

    upgraded spark-core from 1.1.0 to 1.2.0; added run script to build and submit the worscount job

---
+++ mini-complete-example/run.sh
@@ -0,0 +1,4 @@
+sbt clean compile package
+
+spark-submit --class com.oreilly.learningsparkexamples.mini.scala.WordCount target/scala-2.10/learning-spark-mini-example_2.10-0.0.1.jar README.md output
+
